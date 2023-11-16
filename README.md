fb408571868f956d8deb7afcc424277fb4f62897
HanneMaes <maes.hanne@gmail.com>
2023-11-16 12:18:43 +0100

    raspberry pi fixes

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 Node script that takes a screenshot of a webpage at a time internval.
 
 ## Run
-$ node script/paparazzi.js
+1. $ cd script
+2. $ npm install
+1. $ node paparazzi.js
--- script/paparazzi.js
+++ script/paparazzi.js
@@ -5,13 +5,14 @@ const imageWidth  = 1300;
 const imageHeight = 1024;
 
 // time settings
-const interval = true;    // false: take 1 screenshot, true: take screenshots on time 
-const intervalHours = 0;   // these settings only apply when interval == true
-const intervalMinutes = 2;
+const interval = true;     // false: take 1 screenshot, true: take screenshots on time 
+const intervalHours = 1;   // these settings only apply when interval == true
+const intervalMinutes = 45;
 const intervalSeconds = 0;
 
 // technical settings
-const timeoutMinutes = 1; // how long the script waits for the page to load before aborting the script
+const timeoutMinutes = 30; // how long the script waits for the page to load before aborting the script
+const runOnPi = true; // running this script on a PI needs some different options, like chromium instaed of chrome
 
 ///***********************************************************************************************************************************
 // MAIN SCRIPT
@@ -31,9 +32,11 @@ async function takeScreenshot() {
     console.log(dateTimeString + ': loading page...');
 
     // use puppeteer to take a screenshot
-    const browser = await puppeteer.launch();
+    var browserSettings = {};
+    if(runOnPi) { browserSettings = { executablePath: 'chromium-browser' }; }
+    
+    const browser = await puppeteer.launch( browserSettings );
     const page = await browser.newPage();
-    // await page.goto(url);
     await page.goto(url, { timeout: timeoutMinutes * 60 * 1000 }); // the default timeout is 30,000 milliseconds (30 seconds).when the timeout runs out before the page is laoded, no screenshot is taken. So we make the timeout bigger
     await page.setViewport({ width: imageWidth, height: imageHeight });
     await page.screenshot({
@@ -45,13 +48,15 @@ async function takeScreenshot() {
 
 }
 
-// call the asynchronous function
+// take a screenshot
+takeScreenshot();
+
+// take more screenshots if interval == true
 if (interval == true) {
     const intervalMilliseconds = (intervalHours * 60 * 60 + intervalMinutes * 60 + intervalSeconds) * 1000; // convert hours, minutes, and seconds to milliseconds
     setInterval(takeScreenshot, intervalMilliseconds);
-} else {
-    takeScreenshot();
 }
+
 
 //***********************************************************************************************************************************
 // FUNCTIONS
